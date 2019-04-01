3004fce91f65eb746b01907ab450eb7fbeb53baf
Kai Nguyen <kainguyen@student-10-201-43-101.wireless.adelaide.edu.au>
2019-04-01 17:32:41 +1030

    Finish 2 devices contract

--- contracts/ElectricPlugs.sol
+++ contracts/ElectricPlugs.sol
@@ -1,1 +1,41 @@
- 
+pragma solidity ^0.5.6;
+
+contract ElectricPlugs {
+
+    event NameChange(uint electricPlugId, string name);
+    event DescriptionChange(uint electricPlugId, string description);
+    event StatusChange(uint electricPlugId, bool status);
+
+    struct ElectricPlug {
+        string name; // front door 
+        string description; // for plug -> what to, where
+        bool status; // true/false = on/off
+    }
+
+    ElectricPlug[] public electricPlugs;
+
+    mapping (uint => address) public electricPlugToOwner;
+
+    function _addElectricPlug(string memory _name, string memory _description) public {
+        uint _id = electricPlugs.push(ElectricPlug(_name, _description, false)) - 1;
+        electricPlugToOwner[_id] = msg.sender;
+    }
+
+    function _changeName(uint _electricPlugId, string memory _name) public {
+        require(msg.sender == electricPlugToOwner[_electricPlugId]);
+        electricPlugs[_electricPlugId].name = _name;
+        emit NameChange(_electricPlugId, _name);
+    }
+
+    function _changeDescription(uint _electricPlugId, string memory _description) public {
+        require(msg.sender == electricPlugToOwner[_electricPlugId]);
+        electricPlugs[_electricPlugId].description = _description;
+        emit DescriptionChange(_electricPlugId, _description);
+    }
+
+    function _changeStatus(uint _electricplugId, bool _status) public {
+        require(msg.sender == electricPlugToOwner[_electricplugId]);
+        electricPlugs[_electricplugId].status = _status;
+        emit StatusChange(_electricplugId, _status);
+    }
+} 
--- contracts/LightBulbs.sol
+++ contracts/LightBulbs.sol
@@ -2,9 +2,12 @@ pragma solidity ^0.5.6;
 
 contract LightBulbs {
 
-    event NameChange(uint lightbulbId, string name);
-    event StatusChange(uint lightbulbId, bool status);
-    event ColorChange(uint lightbulbId, uint8 red, uint8 green, uint8 blue);
+    event NewLightBulb(uint lightBulbId, string name, string description, bool status, uint8 red, uint8 green, uint8 blue, uint8 intensity);
+    event NameChange(uint lightBulbId, string name);
+    event DescriptionChange(uint lightBulbId, string description);
+    event StatusChange(uint lightBulbId, bool status);
+    event ColorChange(uint lightBulbId, uint8 red, uint8 green, uint8 blue);
+    event IntensityChange(uint lightBulbId, uint8 intensity);
 
     struct LightBulb {
         string name; // front door 
@@ -16,34 +19,45 @@ contract LightBulbs {
         uint8 intensity; // 0-100
     }
 
-    LightBulb[] public lightbulbs;
+    LightBulb[] public lightBulbs;
 
-    mapping (uint => address) public lightbulbToOwner;
+    mapping (uint => address) public lightBulbToOwner;
 
-    function _addNewLightBulb(string memory _name) public {
-        uint _id = lightbulbs.push(LightBulb(_name, false, 255, 255, 255)) - 1;
-        lightbulbToOwner[_id] = msg.sender;
+    function _addNewLightBulb(string memory _name, string memory _description) public {
+        uint _id = lightBulbs.push(LightBulb(_name, _description, false, 255, 255, 255, 100)) - 1;
+        lightBulbToOwner[_id] = msg.sender;
+        emit NewLightBulb(_id, _name, _description, false, 255, 255, 255, 100);
     }
 
-    function _changeName(uint _lightbulbId, string memory _name) public {
-        require(msg.sender == lightbulbToOwner[_lightbulbId]);
-        lightbulbs[_lightbulbId].name = _name;
-        emit NameChange(_lightbulbId, _name);
+    function _changeName(uint _lightBulbId, string memory _name) public {
+        require(msg.sender == lightBulbToOwner[_lightBulbId]);
+        lightBulbs[_lightBulbId].name = _name;
+        emit NameChange(_lightBulbId, _name);
     }
 
-    function _changeStatus(uint _lightbulbId, bool _status) public {
-        require(msg.sender == lightbulbToOwner[_lightbulbId]);
-        lightbulbs[_lightbulbId].status = _status;
-        emit StatusChange(_lightbulbId, _status);
+    function _changeStatus(uint _lightBulbId, bool _status) public {
+        require(msg.sender == lightBulbToOwner[_lightBulbId]);
+        lightBulbs[_lightBulbId].status = _status;
+        emit StatusChange(_lightBulbId, _status);
     }
 
-    function _changeColor(uint _lightbulbId, uint8 _red, uint8 _green, uint8 _blue) public {
-        require(msg.sender == lightbulbToOwner[_lightbulbId]);
-        lightbulbs[_lightbulbId].red = _red;
-        lightbulbs[_lightbulbId].green = _green;
-        lightbulbs[_lightbulbId].green = _blue;
-        emit ColorChange(_lightbulbId, _red, _green, _blue);
+    function _changeDescription(uint _lightBulbId, string memory _description) public {
+        require(msg.sender == lightBulbToOwner[_lightBulbId]);
+        lightBulbs[_lightBulbId].description = _description;
+        emit DescriptionChange(_lightBulbId, _description);
     }
 
+    function _changeColor(uint _lightBulbId, uint8 _red, uint8 _green, uint8 _blue) public {
+        require(msg.sender == lightBulbToOwner[_lightBulbId]);
+        lightBulbs[_lightBulbId].red = _red;
+        lightBulbs[_lightBulbId].green = _green;
+        lightBulbs[_lightBulbId].green = _blue;
+        emit ColorChange(_lightBulbId, _red, _green, _blue);
+    }
 
+    function _changeIntensity(uint _lightBulbId, uint8 _intensity) public {
+        require(msg.sender == lightBulbToOwner[_lightBulbId]);
+        lightBulbs[_lightBulbId].intensity = _intensity;
+        emit IntensityChange(_lightBulbId, _intensity);
+    }
 }
