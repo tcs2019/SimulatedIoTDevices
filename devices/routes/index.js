f239103830f32a399665657689dbb9ac6b6503ab
a1716881 <a1716881@student.adelaide.edu.au>
2019-04-10 01:34:02 +0930

    Reads from redis and renders pages correctly

--- devices/routes/index.js
+++ devices/routes/index.js
@@ -2,116 +2,56 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-
 var redis = require('redis')
 var client = redis.createClient()
 
 client.on('error', function (err) {
   console.log('Error ' + err);
 })
+
+//Connects to Redis server and retrieves data
 client.on('connect', function() {
   console.log('Redis connected');
-  testAdd();
+  getLightbulbs();
+  getAllPlugs();
 });
 
+//Stores objects for use on server
 var AllLightbulbs = [];
 var AllPlugs = [];
 
-//TESTING
-/*
-function testAdd() {
-  client.sadd(['lightbulbs', 'lightbulb1', 'lightbulb2'], function(err, reply) {
-    console.log(reply);
-    client.hmset('lightbulb1', {'id': 'lightbulb1',
-    'name': 'Smart Lightbulb 1',
-    'description': 'This is a lightbulb',
-    'status': 1, 
-    'red': 255,
-    'green': 255,
-    'blue': 0,
-    'intensity': 100
-    }, function(err, reply) {
-        console.log("set" + reply);
-         getLightbulbs();
-         getAllPlugs();
-    });           
-client.hmset('lightbulb2', {'id': 'lightbulb2',
-    'name': 'Smart Lightbulb 2',
-    'description': 'This is a lightbulb, the second one',
-    'status': 1, 
-    'red': 255,
-    'green': 50,
-    'blue': 255,
-    'intensity': 80
-    }, function(err, reply) {
-        console.log("set" + reply);
-         getLightbulbs();
-         getAllPlugs();
-    });   
-  });
-  client.sadd(['plugs', 'plug1', 'plug2'], function(err, reply) {
-    console.log(reply);
-    client.hmset('plug1', {'id': 'plug1',
-    'name': 'Smart Plug 1',
-    'description': 'This is a plug that is on',
-    'status': 1, 
-    }, function(err, reply) {
-        console.log("set" + reply);
-         getLightbulbs();
-         getAllPlugs();
-    });    
-    client.hmset('plug2', {'id': 'plug1',
-    'name': 'Smart Plug 2',
-    'description': 'This is a plug that is off',
-    'status': 0, 
-    }, function(err, reply) {
-        console.log("set" + reply);
-         getLightbulbs();
-         getAllPlugs();
-    });               
-  });  
-                                              
-}
-*/
-
+//Gets data for all lightbulbs and stores it
 function getLightbulbs() {
-  AllLightbulbs = [];
-  var lightbulbHashList = [];
   client.smembers('lightbulbs', function(err, reply) {
+    AllLightbulbs = [];
+    var lightbulbHashList = [];
     lightbulbHashList = reply;
-    console.log("lbs" + reply);
     for(i in lightbulbHashList) {
       client.hgetall(lightbulbHashList[i], function(err, object) {
-        console.log("lb " + i + object.description);
         AllLightbulbs.push(object);
-        console.log(object.name);
       });
     }
   });
 }
 
+//Gets data for all plugs and stores it
 function getAllPlugs() {
+  client.smembers('plugs', function(err, reply) {
   AllPlugs = [];
   var plugHashList = [];
-  client.smembers('plugs', function(err, reply) {
     plugHashList = reply;
-    console.log("pgs" + reply);
     for(i in plugHashList) {
       client.hgetall(plugHashList[i], function(err, object) {
-        console.log("pg " + i + object.description);
         AllPlugs.push(object);
-        console.log(object.name);
       });
     }
   });
 }
 
-/* GET home page. */
+//Gets home page and renders lists of devices
 router.get('/', function(req, res, next) {
-  // getLightbulbs();
-  // getAllPlugs();
-  console.log(AllLightbulbs);
-  console.log(AllPlugs);
+   getLightbulbs();
+   getAllPlugs();
   res.render('index', {lightbulbs: AllLightbulbs, plugs: AllPlugs});
 });
 
@@ -139,7 +79,8 @@ router.get('/lightbulb/:id', function(req, res) {
     setStatus = "OFF"
   }
 
-  res.render('lightbulb', { id: req.params.id, 
+  res.render('lightbulb', { 
+    name: currentLightbulb.name, 
     Description: currentLightbulb.description,
     status: setStatus,
     statusColour: setStatusColour,
@@ -151,7 +92,7 @@ router.get('/lightbulb/:id', function(req, res) {
   });
 });
 
-//Renders individual plug page
+//Renders individual plug
 router.get('/plug/:id', function(req, res) {
   getAllPlugs();
 
@@ -164,6 +105,7 @@ router.get('/plug/:id', function(req, res) {
   
   var setStatusColour;
   var setStatus;
+  
   if(currentPlug.status == 1) {
     setStatusColour = "green";
     setStatus = "ON"
@@ -173,11 +115,101 @@ router.get('/plug/:id', function(req, res) {
     setStatus = "OFF"
   }
 
-  res.render('plug', { id: req.params.id, 
-                        Description: currentPlug.description,
-                        status: setStatus,
-                        statusColour: setStatusColour,
-                          });
+  res.render('plug', { 
+    name: currentPlug.name, 
+    Description: currentPlug.description,
+    status: setStatus,
+    statusColour: setStatusColour,
+  });
 });
 
 module.exports = router;
+
+/* TESTING FUNCTIONS
+
+router.get('/lightbulbchange', function(req, res) {
+  client.hmset('lightbulb1', {'id': 'lightbulb1',
+    'name': 'Smart Lightbulb 1',
+    'description': 'This is a lightbulb that was changed',
+    'status': 1, 
+    'red': 0,
+    'green': 255,
+    'blue': 255,
+    'intensity': 100
+    }, function(err, reply) {
+        console.log("set" + reply);
+    }); 
+
+    res.render('index', {lightbulbs: AllLightbulbs, plugs: AllPlugs});
+});
+
+router.get('/plugchange', function(req, res) {
+  client.hmset('plug2', {'id': 'plug2',
+  'name': 'Smart Plug 2',
+  'description': 'This is a plug that is off that is on now',
+  'status': 1, 
+  }, function(err, reply) {
+      console.log("set" + reply);
+  }); 
+
+  res.render('index', {lightbulbs: AllLightbulbs, plugs: AllPlugs});
+});
+
+function testAdd() {
+  client.sadd(['lightbulbs', 'lightbulb1', 'lightbulb2', 'lightbulb3'], function(err, reply) {
+    console.log(reply);
+    client.hmset('lightbulb1', {'id': 'lightbulb1',
+    'name': 'Smart Lightbulb 1',
+    'description': 'This is a lightbulb',
+    'status': 1, 
+    'red': 255,
+    'green': 255,
+    'blue': 0,
+    'intensity': 100
+    }, function(err, reply) {
+        console.log("set" + reply);
+    });        
+    client.hmset('lightbulb3', {'id': 'lightbulb3',
+    'name': 'Smart Lightbulb 3',
+    'description': 'This is a lightbulb 3',
+    'status': 1, 
+    'red': 255,
+    'green': 255,
+    'blue': 0,
+    'intensity': 100
+    }, function(err, reply) {
+        console.log("set" + reply);
+    });     
+client.hmset('lightbulb2', {'id': 'lightbulb2',
+    'name': 'Smart Lightbulb 2',
+    'description': 'This is a lightbulb, the second one',
+    'status': 1, 
+    'red': 255,
+    'green': 50,
+    'blue': 255,
+    'intensity': 80
+    }, function(err, reply) {
+        console.log("set" + reply);
+    });   
+  });
+  client.sadd(['plugs', 'plug1', 'plug2'], function(err, reply) {
+    console.log(reply);
+    client.hmset('plug1', {'id': 'plug1',
+    'name': 'Smart Plug 1',
+    'description': 'This is a plug that is on',
+    'status': 1, 
+    }, function(err, reply) {
+        console.log("set" + reply);
+    });    
+    client.hmset('plug2', {'id': 'plug2',
+    'name': 'Smart Plug 2',
+    'description': 'This is a plug that is off',
+    'status': 0, 
+    }, function(err, reply) {
+        console.log("set" + reply);
+    });               
+  });  
+                                              
+}
+
+*/
