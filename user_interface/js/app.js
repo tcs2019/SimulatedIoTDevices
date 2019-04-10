4aa3d589143acdea26c8026df04e0b3d5dbb2440
Liuyue Jiang <48501982+liuyuejiang@users.noreply.github.com>
2019-04-10 16:30:10 +0930

    Add files via upload

--- user_interface/js/app.js
+++ user_interface/js/app.js
@@ -1,180 +1,156 @@
 App = {
-  web3Provider: null,
-  contracts: {},
-  account: 0x0,
-  loading: false,
+    web3Provider: null,
+    contracts: {},
+    account: 0x0,
+    loading: false,
 
-  init: function() {
-    return App.initWeb3();
-  },
 
-  initWeb3: function() {
-    // initialize web3
-    if(typeof web3 !== 'undefined') {
-      //reuse the provider of the Web3 object injected by Metamask
-      App.web3Provider = web3.currentProvider;
-    } else {
-      //create a new provider and plug it directly into our local node
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-    }
-    web3 = new Web3(App.web3Provider);
+    init: function() {
+        return App.initWeb3();
+    },
 
-    App.displayAccountInfo();
+    initWeb3: function() {
+        // initialize web3
+        if (typeof web3 !== 'undefined') {
+            //reuse the provider of the Web3 object injected by Metamask
+            App.web3Provider = web3.currentProvider;
+        } else {
+            //create a new provider and plug it directly into our local node
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        }
+        web3 = new Web3(App.web3Provider);
 
-    return App.initContract();
-  },
+        App.displayAccountInfo();
 
-  displayAccountInfo: function() {
-    web3.eth.getCoinbase(function(err, account) {
-      if(err === null) {
-        App.account = account;
-        $('#account').text(account);
-        web3.eth.getBalance(account, function(err, balance) {
-          if(err === null) {
-            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
-          }
-        })
-      }
-    });
-  },
+        return App.initContract();
+    },
 
-  initContract: function() {
-    $.getJSON('ChainList.json', function(chainListArtifact) {
-      // get the contract artifact file and use it to instantiate a truffle contract abstraction
-      App.contracts.ChainList = TruffleContract(chainListArtifact);
-      // set the provider for our contracts
-      App.contracts.ChainList.setProvider(App.web3Provider);
-      // listen to events
-      App.listenToEvents();
-      // retrieve the article from the contract
-      return App.reloadArticles();
-    });
-  },
+    displayAccountInfo: function() {
 
-  reloadArticles: function() {
-    // avoid reentry
-    if(App.loading) {
-      return;
-    }
-    App.loading = true;
 
-    // refresh account information because the balance might have changed
-    App.displayAccountInfo();
-
-    var chainListInstance;
-
-    App.contracts.ChainList.deployed().then(function(instance) {
-      chainListInstance = instance;
-      return chainListInstance.getArticlesForSale();
-    }).then(function(articleIds) {
-      // retrieve the article placeholder and clear it
-      $('#articlesRow').empty();
-
-      for(var i = 0; i < articleIds.length; i++) {
-        var articleId = articleIds[i];
-        chainListInstance.articles(articleId.toNumber()).then(function(article){
-          App.displayArticle(article[0], article[1], article[3], article[4], article[5]);
+        web3.eth.getCoinbase(function(err, account) {
+            if (err === null) {
+                console.log(account);
+                App.account = account;
+                $('#account').text(account);
+                // web3.eth.getBalance(account, function(err, balance) {
+                //     if (err === null) {
+                //         $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+                //     }
+                // })
+            }
         });
-      }
-      App.loading = false;
-    }).catch(function(err) {
-      console.error(err.message);
-      App.loading = false;
-    });
-  },
+    },
 
-  displayArticle: function(id, seller, name, description, price) {
-    var articlesRow = $('#articlesRow');
+    initContract: function() {
+        $.getJSON('LightBulbs.json', function(ioTDevicesArtifact) {
+            // get the contract artifact file and use it to instantiate a truffle contract abstraction
+            App.contracts.IoTDevices = TruffleContract(ioTDevicesArtifact);
+            // set the provider for our contracts
+            App.contracts.IoTDevices.setProvider(App.web3Provider);
+            // listen to events
+            App.listenToEvents();
+            // retrieve the device from the contract
+            return App.reloadDevicesList();
+        });
+    },
 
-    var etherPrice = web3.fromWei(price, "ether");
-
-    var articleTemplate = $("#articleTemplate");
-    articleTemplate.find('.panel-title').text(name);
-    articleTemplate.find('.article-description').text(description);
-    articleTemplate.find('.article-price').text(etherPrice + " ETH");
-    articleTemplate.find('.btn-buy').attr('data-id', id);
-    articleTemplate.find('.btn-buy').attr('data-value', etherPrice);
-
-    // seller
-    if (seller == App.account) {
-      articleTemplate.find('.article-seller').text("You");
-      articleTemplate.find('.btn-buy').hide();
-    } else {
-      articleTemplate.find('.article-seller').text(seller);
-      articleTemplate.find('.btn-buy').show();
-    }
-
-    // add this new article
-    articlesRow.append(articleTemplate.html());
-  },
-
-  sellArticle: function() {
-    // retrieve the detail of the article
-    var _article_name = $('#article_name').val();
-    var _description = $('#article_description').val();
-    var _price = web3.toWei(parseFloat($('#article_price').val() || 0), "ether");
-
-    if((_article_name.trim() == '') || (_price == 0)) {
-      // nothing to sell
-      return false;
-    }
-
-    App.contracts.ChainList.deployed().then(function(instance) {
-      return instance.sellArticle(_article_name, _description, _price, {
-        from: App.account,
-        gas: 500000
-      });
-    }).then(function(result) {
-
-    }).catch(function(err) {
-      console.error(err);
-    });
-  },
-
-  // listen to events triggered by the contract
-  listenToEvents: function() {
-    App.contracts.ChainList.deployed().then(function(instance) {
-      instance.LogSellArticle({}, {}).watch(function(error, event) {
-        if (!error) {
-          $("#events").append('<li class="list-group-item">' + event.args._name + ' is now for sale</li>');
-        } else {
-          console.error(error);
+    reloadDevicesList: function() {
+        // avoid reentry
+        if (App.loading) {
+            return;
         }
-        App.reloadArticles();
-      });
+        App.loading = true;
 
-      instance.LogBuyArticle({}, {}).watch(function(error, event) {
-        if (!error) {
-          $("#events").append('<li class="list-group-item">' + event.args._buyer + ' bought ' + event.args._name + '</li>');
-        } else {
-          console.error(error);
+        // refresh account information because the balance might have changed
+        App.displayAccountInfo();
+
+        var ioTDevicesInstance;
+
+        App.contracts.IoTDevices.deployed().then(function(instance) {
+            ioTDevicesInstance = instance;
+            return ioTDevicesInstance.getNumberOfdevices();
+        }).then(function(deviceIds) {
+            // retrieve the device placeholder and clear it
+            $('#devicesRow').empty();
+            console.log("Displaying devices");
+
+            for (var i = 0; i < deviceIds; i++) {
+                console.log(deviceIds);
+                ioTDevicesInstance.lightBulbs(i).then(function(device) {
+                    App.displayDevice(device[0], device[1], device[2]);
+                    console.log(device[0]);
+                });
+            }
+            App.loading = false;
+        }).catch(function(err) {
+            console.error(err.message);
+            App.loading = false;
+        });
+    },
+
+    displayDevice: function(id, name, description) {
+        var devicesRow = $('#devicesRow');
+        // var deviceid = $('#deviceid');
+
+        var deviceTemplate = $("#deviceTemplate");
+        deviceTemplate.find('.panel-title').text(name);
+        deviceTemplate.find('.device-description').text(description);
+        deviceTemplate.find('.device-deviceid').text(id);
+
+        // seller
+        // if (seller == App.account) {
+        //     deviceTemplate.find('.device-seller').text("You");
+        //     deviceTemplate.find('.btn-buy').hide();
+        // } else {
+        //     deviceTemplate.find('.device-seller').text(seller);
+        //     deviceTemplate.find('.btn-buy').show();
+        // }
+
+        // add this new device
+        devicesRow.append(deviceTemplate.html());
+    },
+
+    addDevice: function() {
+        // retrieve the detail of the device
+        var _device_name = $('#device_name').val();
+        var _description = $('#device_description').val();
+
+        if ((_device_name.trim() == '')) {
+            // nothing to sell
+            return false;
         }
-        App.reloadArticles();
-      });
-    });
-  },
 
-  buyArticle: function() {
-    event.preventDefault();
+        App.contracts.IoTDevices.deployed().then(function(instance) {
+            console.log(_device_name);
+            return instance._addNewLightBulb(_device_name, _description, {
+                from: App.account,
+                gas: 500000
+            });
+        }).then(function(result) {
 
-    // retrieve the article
-    var _articleId = $(event.target).data('id');
-    var _price = parseFloat($(event.target).data('value'));
+        }).catch(function(err) {
+            console.error(err);
+        });
+    },
 
-    App.contracts.ChainList.deployed().then(function(instance){
-      return instance.buyArticle(_articleId, {
-        from: App.account,
-        value: web3.toWei(_price, "ether"),
-        gas: 500000
-      });
-    }).catch(function(error) {
-      console.error(error);
-    });
-  }
+    // listen to events triggered by the contract
+    listenToEvents: function() {
+        App.contracts.IoTDevices.deployed().then(function(instance) {
+            instance.NewLightBulb({}, {}).watch(function(error, event) {
+                if (!error) {
+                    $("#events").append('<li class="list-group-item">' + event.args._name + ' (' + event.args._description + ' )' + ' is now added</li>');
+                } else {
+                    console.error(error);
+                }
+                App.reloadDevicesList();
+            });
+        });
+    },
 };
 
 $(function() {
-  $(window).load(function() {
-    App.init();
-  });
+    $(window).load(function() {
+        App.init();
+    });
 });
