fcf0c788ce3d9df793d7e7cb5c21689d16b19dbd
Kai Nguyen <kainguyen@student-10-201-42-146.wireless.adelaide.edu.au>
2019-04-12 14:14:30 +0930

    Finish first prototype

--- devices/public/JS/script.js
+++ devices/public/JS/script.js
@@ -1,30 +1,30 @@
-//Updates slider numbers and light bulb colour as sliders are changed
+// Updates slider numbers and light bulb colour as sliders are changed
 function changeBulb() {
-    var r = document.getElementById("RValue").value;
-    var g = document.getElementById("GValue").value;
-    var b = document.getElementById("BValue").value;
-    var i = document.getElementById("IValue").value;
-    
-    document.getElementById("RNumber").innerHTML = r;
-    document.getElementById("GNumber").innerHTML = g;
-    document.getElementById("BNumber").innerHTML = b;
-    document.getElementById("INumber").innerHTML = i;
-    
-    i = (i / 2) / 100;
-    
-    document.getElementById("overlay").style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + i + ")";
+  var r = document.getElementById('RValue').value;
+  var g = document.getElementById('GValue').value;
+  var b = document.getElementById('BValue').value;
+  var i = document.getElementById('IValue').value;
+
+  document.getElementById('RNumber').innerHTML = r;
+  document.getElementById('GNumber').innerHTML = g;
+  document.getElementById('BNumber').innerHTML = b;
+  document.getElementById('INumber').innerHTML = i;
+
+  i /= 100;
+
+  document.getElementById('overlay').style.backgroundColor =
+    'rgba(' + r + ', ' + g + ', ' + b + ', ' + i + ')';
 }
 
-//Changes text and colour of ON/OFF text
+// Changes text and colour of ON/OFF text
 function changeStatus() {
-    if(document.getElementById("deviceStatus").innerHTML == "OFF") {
-        document.getElementById("deviceStatus").innerHTML = "ON";
-        document.getElementById("deviceStatus").style.color = "green";
-        document.getElementById("plug").style.backgroundColor = "green";
-    }
-    else {
-        document.getElementById("deviceStatus").innerHTML = "OFF";
-        document.getElementById("deviceStatus").style.color = "red";
-        document.getElementById("plug").style.backgroundColor = "red";
-    }
+  if (document.getElementById('deviceStatus').innerHTML == 'OFF') {
+    document.getElementById('deviceStatus').innerHTML = 'ON';
+    document.getElementById('deviceStatus').style.color = 'green';
+    document.getElementById('plug').style.backgroundColor = 'green';
+  } else {
+    document.getElementById('deviceStatus').innerHTML = 'OFF';
+    document.getElementById('deviceStatus').style.color = 'red';
+    document.getElementById('plug').style.backgroundColor = 'red';
+  }
 }
